3db0bdad2a440dab8a817dec5c7ff71b33e5b885
Alexandr Suhovoy <a.suhovoy@imi.de>
2017-12-19 10:33:06 +0200

    return token, when new user added to mongo db

---
+++ controllers/authentication.js
@@ -0,0 +1,37 @@
+const jwt = require('jwt-simple');
+const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) { 
+	const timestamp = new Date().getTime();
+	return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signup = function (req, res, next) {
+	//res.send({success: true});
+	const email = req.body.email;
+	const password = req.body.password;
+	
+	if(!email || !password) { 
+		return res.status(422).send({error: 'You must provide email and password'});
+	}
+
+	User.findOne({email: email}, function (err, existUser) {
+		if (err) { return next(err); }
+
+		if (existUser) {
+			return res.status(422).send({error : 'User already exist'});
+		}
+	});
+
+	const user = new User({
+		email: email,
+		password: password
+	});
+
+	user.save(function(err) {
+		if (err) { return next(err); }
+
+		return res.send({token: tokenForUser(user)});
+	});
+}
